cf39ed94dbac2c996f85b486229ba0ae002dc589
Vibhanshu <vibhuuu.22@gmail.com>
2025-08-24 16:29:37 +0530

    New Tile

--- frontend/index.js
+++ frontend/index.js
@@ -176,11 +176,12 @@ function updateMapTiles(theme) {
   //   attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>'
   // }).addTo(map);
 
-  L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     maxZoom: 18,
-    attribution: '© OpenTopoMap contributors',
-    subdomains: ['a', 'b', 'c']
+    attribution: '© CartoDB, © OpenStreetMap contributors',
+    subdomains: ['a', 'b', 'c', 'd']
 }).addTo(map);
+
 
 }
 
